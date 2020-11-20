f418c8c13d3bda49daf2c8dfe269b614c7c65ca8
vagarciave <vagarciave@unal.edu.co>
2020-11-19 23:50:35 -0500

    :+1: cambios finales

--- Shiny_dashboard/app.R
+++ Shiny_dashboard/app.R
@@ -13,18 +13,10 @@ library(raster)
 library(dashboardthemes)
 library(rgdal)
 
-# Cargar mapa
-load('medellin_map.RData',.GlobalEnv)
 
 # Cargar datos
 load(file = 'datos.RData', .GlobalEnv)
-
-# listas para nombre de barrios y comunas
-list_barrios <- sort(unique(datos$BARRIO))
-list_comunas <- sort(unique(datos$COMUNA))
-
-source('source_models.R')
-source('source_map.R')
+load(file = 'medellin_map.RData', .GlobalEnv)
 
 header <-   dashboardHeader(  ### changing logo
   title = shinyDashboardLogo(
@@ -36,24 +28,24 @@ header <-   dashboardHeader(  ### changing logo
   titleWidth = 300
 )
 
+source("source_map.R")
 
 header$children[[2]]$children[[2]] <- header$children[[2]]$children[[1]]
 header$children[[2]]$children[[1]] <- tags$a(tags$img(src='transitapp.png',width = "50%", height = "50%"),
                                              target = '_blank') 
 ui <- dashboardPage(title = "TRANSITApp",
                     header,
-
                     dashboardSidebar(
                       sidebarMenu(
                         menuItem("Inicio", tabName = "inicio", icon = icon("home")),
                         menuItem("Tabla de datos históricos", tabName = "datos", icon = icon("table")),
-                        menuItem("Visualizacion", tabName = "visualizacion", icon = icon("chart-bar")),
+                        menuItem("Visualizacion", href = "https://daniel-chanci-restrepo.shinyapps.io/Visualizacion_accidentalidad/", icon = icon("chart-bar")),
                         menuItem("Accidentalidad", tabName = "acc",icon = icon("car-crash"),
                                  menuSubItem("Accidentalidad comunas",
                                              href = "https://valentina-garcia-velasquez.shinyapps.io/Prediccion_accidentalidad_comuna/"),
                                  menuSubItem("Accidentalidad barrios",
                                              href = "https://valentina-garcia-velasquez.shinyapps.io/Prediccion_accidentalidad_barrio/")),
-                        menuItem("Agrupamiento", tabName = "agrupamiento", icon = icon("map-marked-alt")),
+                        menuItem("Agrupamiento", href = "https://daniel-chanci-restrepo.shinyapps.io/Agrupamiento/", icon = icon("map-marked-alt")),
                         menuItem("Equipo", tabName = "equipo", icon = icon("users"))
                       )
                     ),
@@ -99,62 +91,7 @@ ui <- dashboardPage(title = "TRANSITApp",
                                 fluidRow(box(DT::dataTableOutput("Data")  %>% shinycssloaders::withSpinner(color="#3498db"), 
                                              style = "height:500px; overflow-y: scroll;overflow-x: scroll;",
                                              width=12))
-                        ),
                         
-                        tabItem(tabName = "visualizacion",
-                                h1("Visualización de datos",align = "center"),
-                                fluidRow(
-                                  box(width = 12,
-                                      dateRangeInput("daterange2", "Rango de Tiempo:",
-                                                     start  = "2014-01-01",
-                                                     end    = "2018-12-31",
-                                                     min    = "2014-01-01",
-                                                     max    = "2018-12-31",
-                                                     format = "dd/mm/yyyy",
-                                                     separator = " - ",
-                                                     language = "es")
-                                  )
-                                ),
-                                
-                                fluidRow(
-                                  box(width = 4,
-                                      plotlyOutput("chart_clase")%>% shinycssloaders::withSpinner(color="#3498db")),
-                                  box(width = 4,
-                                      plotlyOutput("chart_gravedad")%>% shinycssloaders::withSpinner(color="#3498db")),
-                                  box(width = 4,
-                                      plotlyOutput("chart_diseno")%>% shinycssloaders::withSpinner(color="#3498db"))
-                                  
-                                ),
-                                fluidRow(
-                                  box(width = 6,
-                                      plotlyOutput("chart_comuna")%>% shinycssloaders::withSpinner(color="#3498db")),
-                                  box(width = 6,
-                                      plotlyOutput("chart_barrio")%>% shinycssloaders::withSpinner(color="#3498db"))
-                                )
-                                
-                        ),
-                        
-                        # Second tab content
-                        tabItem(tabName = "acc"
-                                
-                        ),
-                        # Second tab content
-                        tabItem(tabName = "agrupamiento",
-                                h1("Agrupamiento de barrios en Medellín"),
-                                h5("El siguiente mapa muestra los barrios de Medellín agrupados por similaridades, cada grupo tiene características
-                                diferentes de acuerdo al número de accidentes que se presentaron del 2014 al 2018."),
-                                h5("Puede seleccionar un barrio en el mapa y obtener información histórica del mismo, además se presenta una tabla 
-                                   con la información de cada grupo."),
-                                fluidPage(
-                                  column(width = 12,
-                                         box(width = NULL, solidHeader = TRUE,
-                                             leafletOutput("mymap")  %>% shinycssloaders::withSpinner(color="#3498db"),
-                                             p()
-                                         ),
-                                         box(width = NULL,
-                                             uiOutput("clusters_table") %>% shinycssloaders::withSpinner(color="#3498db"))
-                                  )
-                                )
                         ),
                         tabItem(tabName = "equipo",
                                 h2("Daniel Chanci Restrepo", align = "center"),
@@ -183,173 +120,15 @@ server <- function(input, output) {
   #Datos de visualizacion
   output$Data <- DT::renderDataTable(
     DT::datatable({
-      datos %>% filter(ymd(FECHA) >= input$daterange[1], ymd(FECHA) <= input$daterange[2]) %>%
+      datos %>% filter(ymd(FECHA) >= input$daterange[1], ymd(FECHA) <= input$daterange[2]) %>% 
         group_by(FECHA,CLASE,GRAVEDAD,COMUNA,BARRIO) %>%
-        summarise("TOTAL ACCIDENTES" = n())
+         summarise("TOTAL ACCIDENTES" = n())
     },
     options = list(lenghtMenu = list(c(7, 15, -1), c('5', '15', 'All')), pageLenght = 15),
     filter = "top",
-    selection = "multiple",
     style = "bootstrap"
     ))
-  
-  # Mapa
-  output$mymap <- renderLeaflet({
-    create_map()
-  })
-################Tabla para el mapa########
-  output$clusters_table <- renderUI({
-    dirColors <-c("1"="#EE3E32", "2"="#F68838", "3"="#FBB021", "4"="#1B8A5A", "5"="#D0C7C7")
-    
-    # Create a Bootstrap-styled table
-    tags$table(class = "table",
-               tags$thead(tags$tr(
-                 tags$th("Color"),
-                 tags$th("Nombre"),
-                 tags$th("Promedio de muertos"),
-                 tags$th("Promedio de heridos"),
-                 tags$th("Promedio de solo daños")
-                 
-               )),
-               tags$tbody(
-                 tags$tr(
-                   tags$td(span(style = sprintf(
-                     "width:1.1em; height:1.1em; background-color:%s; display:inline-block;",
-                     dirColors[1]
-                   ))),
-                   tags$td("Accidentalidad alta"),
-                   tags$td('23.44'),
-                   tags$td('1773.11'),
-                   tags$td('2149.11')
-                 ),
-                 tags$tr(
-                   tags$td(span(style = sprintf(
-                     "width:1.1em; height:1.1em; background-color:%s; display:inline-block;",
-                     dirColors[2]
-                   ))),
-                   tags$td("Accidentalidad media"),
-                   tags$td('12.35'),
-                   tags$td('1050.35'),
-                   tags$td('1142.87')
-                 ),
-                 tags$tr(
-                   tags$td(span(style = sprintf(
-                     "width:1.1em; height:1.1em; background-color:%s; display:inline-block;",
-                     dirColors[3]
-                   ))),
-                   tags$td("Accidentalidad moderada"),
-                   tags$td('5.3'),
-                   tags$td('550.34'),
-                   tags$td('382.37')
-                 ),
-                 tags$tr(
-                   tags$td(span(style = sprintf(
-                     "width:1.1em; height:1.1em; background-color:%s; display:inline-block;",
-                     dirColors[4]
-                   ))),
-                   tags$td("Accidentalidad baja"),
-                   tags$td('1.71'),
-                   tags$td('171.6'),
-                   tags$td('104.78')
-                 ),
-                 tags$tr(
-                   tags$td(span(style = sprintf(
-                     "width:1.1em; height:1.1em; background-color:%s; display:inline-block;",
-                     dirColors[5]
-                   ))),
-                   tags$td("Sin información"),
-                   tags$td("Sin información"),
-                   tags$td("Sin información"),
-                   tags$td("Sin información")
-                 )
-               )
-    )
-  })
-########### Para gráficos del dash ########
-  output$chart_clase <- renderPlotly({
-    datos %>% filter(ymd(FECHA) >= input$daterange2[1],
-                     ymd(FECHA) <= input$daterange2[2]) %>%
-      group_by(CLASE) %>% 
-      summarise(TOTAL = n())  %>% arrange(desc(TOTAL)) %>%
-      plot_ly(x = ~ CLASE, y = ~ TOTAL,color = ~CLASE, type = 'bar')   %>%
-      layout(
-        title = "Total de accidentes por Clase",
-        xaxis = list(title = "",
-                     categoryorder = "array",
-                     categoryarray = ~CLASE),
-        yaxis = list(title = "Total")
-      )
-  })
-  # Grafico gravedad
-  output$chart_gravedad<- renderPlotly({
-    datos %>% filter(ymd(FECHA) >= input$daterange2[1],
-                     ymd(FECHA) <= input$daterange2[2]) %>%
-      group_by(GRAVEDAD) %>% 
-      summarise(TOTAL = n())  %>% arrange(desc(TOTAL)) %>%
-      plot_ly(labels = ~GRAVEDAD, values = ~TOTAL, type = 'pie') %>% 
-      layout(title = "Total de accidentes por gravedad",
-             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-  })
-  #Grafico comuna
-  output$chart_comuna <- renderPlotly({
-    datos %>% filter(ymd(FECHA) >= input$daterange2[1],
-                     ymd(FECHA) <= input$daterange2[2]) %>%
-      group_by(COMUNA) %>% 
-      summarise(TOTAL = n())  %>% arrange(desc(TOTAL)) %>%
-      plot_ly(x = ~ COMUNA, y = ~ TOTAL,color = ~COMUNA, type = 'bar')  %>%
-      layout(
-        title = "Total de accidentes por Comuna",
-        xaxis = list(title = "",
-                     categoryorder = "array",
-                     categoryarray = ~COMUNA),
-        yaxis = list(title = "Total")
-      )
-  })
-  
-  #Grafico comuna
-  output$chart_barrio <- renderPlotly({
-    datos %>% filter(ymd(FECHA) >= input$daterange2[1],
-                     ymd(FECHA) <= input$daterange2[2]) %>%
-      group_by(BARRIO) %>% 
-      summarise(TOTAL = n())  %>% arrange(desc(TOTAL)) %>%
-      top_n(10) %>%
-      plot_ly(x = ~ BARRIO, y = ~ TOTAL,color = ~BARRIO, type = 'bar')  %>%
-      layout(
-        title = "Top 10 barrios con más accidentes",
-        xaxis = list(title = "",
-                     categoryorder = "array",
-                     categoryarray = ~BARRIO),
-        yaxis = list(title = "Total")
-      )
-  })
-  
-  # Grafico diseño
-  output$chart_diseno <- renderPlotly(({
-    df_diseno <- datos %>% filter(ymd(FECHA) >= input$daterange2[1],
-                        ymd(FECHA) <= input$daterange2[2]) %>%
-      group_by(DISENO) %>% 
-      summarise(TOTAL = n())  %>% arrange(desc(TOTAL)) %>%
-      arrange(desc(TOTAL))
-    fig <- plot_ly(
-      type = 'table',
-      header = list(
-        values = c('<b>DISEÑO</b>', '<b>Total</b>'),
-        line = list(color = '#506784'),
-        fill = list(color = '#119DFF'),
-        align = c('left','center'),
-        font = list(color = 'white', size = 12)
-      ),
-      cells = list(
-        values = t(df_diseno),
-        line = list(color = '#506784'),
-        fill = list(color = c('#d0eeec', 'white')),
-        align = c('left', 'center'),
-        font = list(color = c('black'), size = 12)
-      ))
-    fig
-  }))
-  
+
 }
 
 shinyApp(ui, server)
